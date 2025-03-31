250c8d24aba283d08288c315f3855d386b4f2362
PhuNN <phunn280604@gmail.com>
2025-03-31 23:10:38 +0700

    Update checkout function

--- Function/checkoutProcessF.js
+++ Function/checkoutProcessF.js
@@ -1,18 +1,37 @@
-const { By } = require("selenium-webdriver");
-var { readGlobalData } = require("./globalF")
+const { By, until } = require("selenium-webdriver");
+var { readGlobalData } = require("./globalF");
 
-var PayPass01 = async function (driver) {
+var TestPaymentWithoutLogin = async function (driver) {
   try {
 
     let globalD = await readGlobalData("global")
     await driver.get(globalD.data.url)
-    let getDiv = await driver.findElement(By.css("//div[@class = 'grid__item swiper-slide swiper-slide-prev']"))
-    let linksPro = await getDiv.getAttribute("href")
-    console.log(linksPro)
+    await driver
+      .findElement(By.xpath("//input[contains(@Class, 'search__input field__input boost-sd__search-widget-init-input')]"))
+      .sendKeys("robot")
+    let firstPro = await driver
+      .wait(until
+        .elementLocated(By.xpath(
+          "//ul[@class='boost-sd__instant-search-product-list-items']//li")), 
+          5000);
+    let urlPro = await driver
+      .findElement(By.xpath("//a[@class='boost-sd__suggestion-queries-item-link']"))
+      .getAttribute("href");
+    await driver.get(urlPro);
+    let addCardB = await driver
+      .wait(until.elementsLocated(
+        By.xpath(
+          "//div[@data-meta='__']//div//div//div//product-form [@data-hide-errors='false']//form[@method='post']//div//button[@name='add']",
+          5000)));
+    await addCardB.click();
+
+    let cardB = await driver.findElement(By.xpath("//*[name()='path' and contains(@d,'M23.1463 1')]"));
+    cardB.click();
+    console.log(button);
 
   } catch (error) {
     console.error(`PAY-PASS01: ${error.message}`);
-    throw new Error(`PAY-PASS02: ${error.message}`);
+    throw new Error(`PAY-PASS01: ${error.message}`);
   }
 };
 
@@ -26,6 +45,6 @@ var PayPass02 = async function (driver) {
 };
 
 module.exports = {
-  PayPass01,
+  TestPaymentWithoutLogin,
   PayPass02
 }
--- Index/checkoutProcessI.js
+++ Index/checkoutProcessI.js
@@ -1,16 +1,18 @@
 const { Builder, Browser } = require("selenium-webdriver");
-var { payPass01, payPass02 } = require("../Function/checkoutProcessF");
+var { TestPaymentWithoutLogin, PayPass02 } = require("../Function/checkoutProcessF");
 
-var PayPass01 = async function(){
+var TestPayPass01 = async function(){
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try{
-        payPass01(driver)
+        await TestPaymentWithoutLogin(driver);
     }
     catch(error){
-        console.log(error)
+        console.log(error);
     }
     finally{
-        driver.quit()
-        console.log("PAY-PASS01 exit ...")
+        //driver.quit()
+        console.log("PAY-PASS01 exit ...");
     }
 }
+
+TestPayPass01();
