7ba13cfc10607af111660f0b9a9f1cd8d9e82dc6
PhuNN <phunn280604@gmail.com>
2025-04-01 15:33:18 +0700

    Done testcase register

--- Function/registerF.js
+++ Function/registerF.js
@@ -95,6 +95,162 @@ var TestRegisterEmpty01 = async function (driver) {
       }
 };
 
+var TestRegisterEmpty02 = async function (driver) {
+    try {
+
+        // Step 1.
+        await Step1(driver);
+
+        // Step 2.
+        await Step2(driver);
+
+        // Step 3.
+        await Step3(driver);
+
+        let regPass01 = await readGlobalData("register", "REG-EMPTY02");
+
+        // Step 4.
+        await StepInputField("//input[@id='RegisterForm-FirstName']", driver, regPass01.data.firstName);
+
+        // Step 5.
+        await StepInputField("//input[@id='RegisterForm-LastName']", driver, regPass01.data.lastName);
+
+        // Step 6.
+        await StepInputField("//input[@id='RegisterForm-phone']", driver, regPass01.data.phoneNumber);
+
+        // Step 7.
+        await StepComboBox("//select[@id='gender']", driver, regPass01.data.sex);
+
+        // Step 8.
+        await StepInputField("//input[@id='RegisterForm-email']", driver, regPass01.data.email);
+
+        // Step 9.
+        await StepInputField("//input[@id='RegisterForm-password']", driver, regPass01.data.password);
+
+        // Step 10.
+        await StepInputField("//input[@id='confirm-password']", driver, regPass01.data.repassword);
+
+        // Step 11.
+        await StepCheckBox("//input[@name='check-box'][@id='agree-terms-acc']", driver);
+
+        await Step12("//button[@id='register-button']", driver);
+
+        await driver.sleep(10000);
+    
+      } catch (error) {
+        throw new Error(`REG-PASS02: ${error.message}`);
+      }
+};
+
+var TestRegisterError01 = async function (driver) {
+    try {
+        // Step 1.
+        await Step1(driver);
+
+        // Step 2.
+        await Step2(driver);
+
+        // Step 3.
+        await Step3(driver);
+
+        let regPass01 = await readGlobalData("register", "REG-ERR01");
+
+        // Step 4.
+            await StepInputField("//input[@id='RegisterForm-FirstName']", driver, regPass01.data.firstName);
+
+            // Step 5.
+            await StepInputField("//input[@id='RegisterForm-LastName']", driver, regPass01.data.lastName);
+
+            // Step 7.
+            await StepComboBox("//select[@id='gender']", driver, regPass01.data.sex);
+
+            // Step 8.
+            await StepInputField("//input[@id='RegisterForm-email']", driver, regPass01.data.email);
+        
+            // Step 9.
+            await StepInputField("//input[@id='RegisterForm-password']", driver, regPass01.data.password);
+
+            // Step 10.
+            await StepInputField("//input[@id='confirm-password']", driver, regPass01.data.repassword);
+
+            // Step 11.
+            await StepCheckBox("//input[@name='check-box'][@id='agree-terms-acc']", driver);
+        for (let item of regPass01.data.phoneNumber){
+
+            let phoneId = item.id;
+            let phoneValue = item.value;
+
+            // Step 6.
+            console.log(`Case: ${phoneId}`)
+            await StepInputField("//input[@id='RegisterForm-phone']", driver, phoneValue);
+
+            await Step12("//button[@id='register-button']", driver);
+
+            let alert = await driver.switchTo().alert();
+            await alert.accept();
+        }
+        await driver.sleep(10000);
+    
+      } catch (error) {
+        throw new Error(`REG-ERR01: ${error.message}`);
+      }
+};
+
+var TestRegisterError02 = async function (driver) {
+    try {
+        // Step 1.
+        await Step1(driver);
+
+        // Step 2.
+        await Step2(driver);
+
+        // Step 3.
+        await Step3(driver);
+
+        let regErr02 = await readGlobalData("register", "REG-ERR02");
+
+        // Step 4.
+            await StepInputField("//input[@id='RegisterForm-FirstName']", driver, regErr02.data.firstName);
+
+            // Step 5.
+            await StepInputField("//input[@id='RegisterForm-LastName']", driver, regErr02.data.lastName);
+            
+            // Step 7.
+            await StepComboBox("//select[@id='gender']", driver, regErr02.data.sex);
+
+            // Step 6.
+            
+            await StepInputField("//input[@id='RegisterForm-phone']", driver, regErr02.data.phoneNumber);
+        
+            // Step 9.
+            await StepInputField("//input[@id='RegisterForm-password']", driver, regErr02.data.password);
+
+            // Step 10.
+            await StepInputField("//input[@id='confirm-password']", driver, regErr02.data.repassword);
+
+            // Step 11.
+            await StepCheckBox("//input[@name='check-box'][@id='agree-terms-acc']", driver);
+        for (let item of regErr02.data.email){
+
+            let emailId = item.id;
+            let emailValue = item.value;
+
+            // Step 8.
+            console.log(`Case: ${emailId}`)
+            await StepInputField("//input[@id='RegisterForm-email']", driver, emailValue);
+
+            await Step12("//button[@id='register-button']", driver);
+
+            let alert = await driver.switchTo().alert();
+            await alert.accept();
+        }
+        await driver.sleep(10000);
+    
+      } catch (error) {
+        throw new Error(`REG-ERR01: ${error.message}`);
+      }
+};
+
 // Step 1
 var Step1 = async function(driver) {
     var isSuccess = false
@@ -162,6 +318,7 @@ var StepInputField = async function(xpath, driver, key) {
             until.elementLocated(By.xpath(xpath)),
             10000
         );
+        await textField.clear();
         await textField.sendKeys(key);
         isSuccess = true;
     }
@@ -248,5 +405,8 @@ var Step12 = async function(xpath, driver) {
 
 module.exports = {
     TestRegisterPass,
-    TestRegisterEmpty01
+    TestRegisterEmpty01,
+    TestRegisterEmpty02,
+    TestRegisterError01,
+    TestRegisterError02
 }
--- Index/registerI.js
+++ Index/registerI.js
@@ -1,7 +1,12 @@
 const { Builder, Browser } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
-var { TestRegisterPass, TestRegisterEmpty01 } = require("../Function/registerF");
+const { 
+    TestRegisterPass, 
+    TestRegisterEmpty01, 
+    TestRegisterEmpty02, 
+    TestRegisterError01,
+    TestRegisterError02 } = require("../Function/registerF");
 
 var REG_PASS01 = async function () {
     let driver = await new Builder()
@@ -15,7 +20,6 @@ var REG_PASS01 = async function () {
         console.log(error);
     }
     finally{
-        await driver.sleep(10000)
         await driver.quit()
         console.log("REG-PASS01 exit ...");
     }
@@ -33,15 +37,68 @@ var REG_EMPTY01 = async function () {
         console.log(error);
     }
     finally{
-        await driver.sleep(10000)
         await driver.quit()
         console.log("REG-EMPTY01 exit ...");
     }
 };
 
+var REG_EMPTY02 = async function () {
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .build();
+    try{
+        await TestRegisterEmpty02(driver);
+        await console.log("TestCase: REG-EMPTY02 is successful!");  
+    }
+    catch(error){
+        console.log(error);
+    }
+    finally{
+        await driver.quit()
+        console.log("REG-EMPTY02 exit ...");
+    }
+};
+
+var REG_ERR01 = async function () {
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .build();
+    try{
+        await TestRegisterError01(driver);
+        await console.log("TestCase: REG-ERR01 is successful!");  
+    }
+    catch(error){
+        console.log(error);
+    }
+    finally{
+        await driver.quit()
+        console.log("REG-ERR01 exit ...");
+    }
+};
+
+var REG_ERR02 = async function () {
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .build();
+    try{
+        await TestRegisterError02(driver);
+        await console.log("TestCase: REG-ERR02 is successful!");  
+    }
+    catch(error){
+        console.log(error);
+    }
+    finally{
+        await driver.quit()
+        console.log("REG-ERR02 exit ...");
+    }
+};
+
 var RunTestCase = async function () {
-    //await REG_PASS01();
+    await REG_PASS01();
     await REG_EMPTY01();
+    await REG_EMPTY02();
+    await REG_ERR01();
+    await REG_ERR02();
 };
 
 RunTestCase();
