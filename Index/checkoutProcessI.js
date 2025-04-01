2f517a2ee81087bee1ad82bb081e43ef02aa7286
PhuNN <phunn280604@gmail.com>
2025-04-01 12:07:19 +0700

    delete file all login

--- Index/checkoutProcessI.js
+++ Index/checkoutProcessI.js
@@ -1,7 +1,7 @@
 const { Builder, Browser } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
-var { TestPaymentWithoutLogin, TestPaymentWithLogin } = require("../Function/checkoutProcessF");
+var { TestPaymentWithoutLogin } = require("../Function/checkoutProcessF");
 
 var TestPayPass01 = async function(){
 
@@ -16,32 +16,14 @@ var TestPayPass01 = async function(){
         console.log(error);
     }
     finally{
-        driver.quit()
+        await driver.sleep(10000)
+        await driver.quit()
         console.log("PAY-PASS01 exit ...");
     }
 }
 
-var TestPayPass02 = async function(){
-
-    let driver = await new Builder()
-        .forBrowser(Browser.CHROME)
-        .build();
-    try{
-        await TestPaymentWithLogin(driver);   
-        await console.log("TestCase: PAY-PASS01 is successful!")           
-    }
-    catch(error){
-        console.log(error);
-    }
-    finally{
-        driver.quit()
-        console.log("PAY-PASS02 exit ...");
-    }
-}
-
 var TestPayCheckout = async function () {
-    //await TestPayPass01();
-    await TestPayPass02();
+    await TestPayPass01();
 }
 
 TestPayCheckout();
