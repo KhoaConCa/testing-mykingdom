ef6ffb9f13ea217ae638833740b2992cc4c153ae
PhuNN <phunn280604@gmail.com>
2025-04-01 14:48:13 +0700

    Update Reg-Empty01

--- Function/registerF.js
+++ Function/registerF.js
@@ -48,6 +48,53 @@ var TestRegisterPass = async function (driver) {
       }
 };
 
+var TestRegisterEmpty01 = async function (driver) {
+    try {
+
+        // Step 1.
+        await Step1(driver);
+
+        // Step 2.
+        await Step2(driver);
+
+        // Step 3.
+        await Step3(driver);
+
+        let regPass01 = await readGlobalData("register", "REG-EMPTY01");
+
+        // Step 4.
+        await StepInputField("//input[@id='RegisterForm-FirstName']", driver, regPass01.data.firstName);
+
+        // Step 5.
+        await StepInputField("//input[@id='RegisterForm-LastName']", driver, regPass01.data.lastName);
+
+        // Step 6.
+        await StepInputField("//input[@id='RegisterForm-phone']", driver, regPass01.data.phoneNumber);
+
+        // Step 7.
+        await StepComboBox("//select[@id='gender']", driver, regPass01.data.sex);
+
+        // Step 8.
+        await StepInputField("//input[@id='RegisterForm-email']", driver, regPass01.data.email);
+
+        // Step 9.
+        await StepInputField("//input[@id='RegisterForm-password']", driver, regPass01.data.password);
+
+        // Step 10.
+        await StepInputField("//input[@id='confirm-password']", driver, regPass01.data.repassword);
+
+        // Step 11.
+        await StepCheckBox("//input[@name='check-box'][@id='agree-terms-acc']", driver);
+
+        await Step12("//button[@id='register-button']", driver);
+
+        await driver.sleep(10000);
+    
+      } catch (error) {
+        throw new Error(`REG-PASS01: ${error.message}`);
+      }
+};
+
 // Step 1
 var Step1 = async function(driver) {
     var isSuccess = false
@@ -61,7 +108,7 @@ var Step1 = async function(driver) {
         throw new Error(`Step 1: ${error.message}`);
     }
     finally{
-        driver.sleep(5000);
+        await driver.sleep(5000);
         return isSuccess;
     }
 };
@@ -82,7 +129,7 @@ var Step2 = async function(driver) {
         throw new Error(`Step 2: ${error.message}`);
     }
     finally{
-        driver.sleep(5000);
+        await driver.sleep(5000);
         return isSuccess;
     }
 };
@@ -103,7 +150,7 @@ var Step3 = async function(driver) {
         throw new Error(`Step 3: ${error.message}`);
     }
     finally{
-        driver.sleep(5000);
+        await driver.sleep(5000);
         return isSuccess;
     }
 };
@@ -123,7 +170,7 @@ var StepInputField = async function(xpath, driver, key) {
         throw new Error(`Step input field: ${error.message}`);
     }
     finally{
-        driver.sleep(2000);
+        await driver.sleep(2000);
         return isSuccess;
     }
 };
@@ -153,7 +200,7 @@ var StepComboBox = async function(xpath, driver, item) {
         throw new Error(`Step combo box: ${error.message}`);
     }
     finally{
-        driver.sleep(2000);
+        await driver.sleep(2000);
         return isSuccess;
     }
 };
@@ -173,7 +220,7 @@ var StepCheckBox = async function(xpath, driver) {
         throw new Error(`Step check box: ${error.message}`);
     }
     finally{
-        driver.sleep(2000);
+        await driver.sleep(2000);
         return isSuccess;
     }
 };
@@ -193,12 +240,13 @@ var Step12 = async function(xpath, driver) {
         throw new Error(`Step 12: ${error.message}`);
     }
     finally{
-        driver.sleep(2000);
+        await driver.sleep(2000);
         return isSuccess;
     }
 };
 
 
 module.exports = {
-    TestRegisterPass
+    TestRegisterPass,
+    TestRegisterEmpty01
 }
